c9bba598b63133ced3cabff3b4f7c161650f9ac7
Shreyash-Adhikari1 <159526048+Shreyash-Adhikari1@users.noreply.github.com>
2024-04-29 23:18:40 +0545

    Delete week3 directory

--- week3/w3task1.java
+++
@@ -1,9 +0,0 @@
-
-public class w3task1 {
-    int num1=12, num2=13, num3=15;
-    if (num1>num2>num3){
-        System.out.println("The maximum among the three is "+num1);
-    }
-    else if(num2>num1>num3);
-        System.out.println("");
-}
